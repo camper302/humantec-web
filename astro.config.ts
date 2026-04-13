8983b3d67193d8c607a15fc09f3da1035df0f71f
camper302 <128845862+camper302@users.noreply.github.com>
2026-04-13 21:57:46 +0900

    Update astro.config.ts

--- astro.config.ts
+++ astro.config.ts
@@ -24,8 +24,6 @@ const whenExternalScripts = (items: (() => AstroIntegration) | (() => AstroInteg
 export default defineConfig({
   output: 'static',
   //시작) 브런치 동작 확인을 위한 소스 향후 머지시 삭제
-  site: 'https://camper302.github.io', 
-  trailingSlash: 'always', 
   // 끝) .github/workflows/deploy.yml 파일도 삭제 필요
   integrations: [
     tailwind({
